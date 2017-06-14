8da04d3d82f65de7c75b8a5020fe9166f1f3df6d
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2017-06-14 09:24:26 +0600

    Update URI_1036.java

--- URI_1036.java
+++ URI_1036.java
@@ -14,22 +14,20 @@ package URI_Problems_solution;
  */
 import java.util.Scanner;
 
-//Problem
-
-public class URI_1036 {
+public class Main {
 
 	public static void main(String[] args) {
-		float A, B, C, R1, R2;
+		double A, B, C, R1, R2;
 		Scanner input =new Scanner(System.in);
-		A = input.nextFloat();
-		B = input.nextFloat();
-		C = input.nextFloat();
+		A = input.nextDouble();
+		B = input.nextDouble();
+		C = input.nextDouble();
 		
 		if ((A == 0) || (((B*B) -(4*A*C)) < 0)) {
 			System.out.print("Impossivel calcular\n");
 		}else {
-			R1 =(float) ((-B + Math.sqrt(((B*B) -(4*A*C)))) / (2*A));
-			R2 =(float) ((-B - Math.sqrt(((B*B) -(4*A*C)))) / (2*A));
+			R1 = ((-B + Math.sqrt(((B*B) -(4*A*C)))) / (2*A));
+			R2 = ((-B - Math.sqrt(((B*B) -(4*A*C)))) / (2*A));
 			
 			System.out.printf("R1 = %.5f\n", R1);
 			System.out.printf("R2 = %.5f\n", R2);
